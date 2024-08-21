a4e5b81d50c26a3911cc3f7b40e08634f2b3f73e
Chris Dexter <8195526+the-dext@users.noreply.github.com>
2024-08-21 16:51:30 +0100

    first 2 challenges

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,66 @@
 // Write your code here
+class Book {
+    title;
+    author;
+    isbn;
+    numCopies;    
+
+    constructor (title, author, isbn, numCopies) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.numCopies = numCopies;
+    }
+
+    getAvailability = () => {
+        if (this.numCopies === 0) {
+            return "out of stock";
+        } else if (this.numCopies > 0 && this.numCopies < 10) {
+            return "low stock";
+        } else {
+            return "in stock";
+        }
+    }
+
+    sell = (numSold = 1) => this.numCopies -= numSold;
+
+    restock = (numCopies = 5) => this.numCopies += numCopies;
+}
+
+let bookInStock = new Book(); 
+bookInStock.numCopies = 11;
+
+let bookLowStock = new Book(); 
+bookLowStock.numCopies = 9;
+
+let bookOutOfStock = new Book(); 
+bookOutOfStock.numCopies = 0;
+
+console.log(bookInStock.getAvailability());
+console.log(bookLowStock.getAvailability());
+console.log(bookOutOfStock.getAvailability());
+
+console.log("Selling books")
+
+console.log(bookInStock.numCopies);
+console.log("sold 1..")
+bookInStock.sell();
+console.log(bookInStock.numCopies);
+
+console.log("sold 2 more..")
+bookInStock.sell(2);
+console.log(bookInStock.numCopies);
+
+
+console.log("restock 3..")
+bookInStock.restock(3);
+console.log(bookInStock.numCopies);
+
+console.log("restock 5..")
+bookInStock.restock();
+console.log(bookInStock.numCopies);
+
+console.log("constructor..")
+let constructorBook = new Book("test book", "author is me", "123abc", 7);
+console.log(constructorBook);
+
--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,18 @@
 // Write your code here
+class Movie {
+    constructor(title, director, genre, releaseYear, rating) {
+        this.title = title;
+        this.director = director;
+        this.genre = genre;
+        this.releaseYear = releaseYear;
+        this.rating = rating;
+    }
+
+    getOverview = () => `${this.title}, a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}`;
+}
+
+let movie = new Movie("wealthify sucks", "chris", "true crime", "2024", "0");
+let movie2 = new Movie("Sopra is cancelled", "chris", "thriller", "2024", "3");
+
+console.log(movie.getOverview());
+console.log(movie2.getOverview());
