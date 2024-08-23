4f50f545af850cac204a97891f5ae39a64aca045
chris dexter <8195526+the-dext@users.noreply.github.com>
2024-08-23 20:13:20 +0100

    comments

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,12 +1,11 @@
 // Write your code here
-function old_getStudents(hasTeachingAssistant, classList) {    
-    let students = [...classList];
+function old_getStudents(hasTeachingAssistant, classList) {
+  let students = [...classList];
+  const _ = students.shift();
+  if (hasTeachingAssistant) {
     const _ = students.shift();
-    if (hasTeachingAssistant)
-    {
-        const _ = students.shift();
-    }
-    return students;
+  }
+  return students;
 }
 /*
 const classList = ['chris', 'sarah', 'ebony', 'tom', 'charlie'];
@@ -18,30 +17,31 @@ let test2 = getStudents(false, classList);
 console.log(test2);
 */
 
-function getStudents(classroom) {    
-    let { hasTeachingAssistant, classList} = classroom;
+function getStudents(classroom) {
+  // object destructuring matches by property name, so hasTeachingAssistant becomes the value of classroom.hasTeachingAssistant
+  let { hasTeachingAssistant, classList } = classroom;
 
-    if (hasTeachingAssistant) {
-        [teacher, teachingAssistant, ...students] = classList;
-    }
-    else {
-        [teacher, ...students] = classList;
-    }
+  // array destructuring takes first element and puts into into the first variable, 2nd into 2nd and so on
+  // until spread with ... takes the rest and puts it into a variable as an array
+  if (hasTeachingAssistant) {
+    [teacher, teachingAssistant, ...students] = classList;
+  } else {
+    [teacher, ...students] = classList;
+  }
 
-    return students;
+  return students;
 }
 
-
-
 const classroomWithTeachingAssistant = {
-    hasTeachingAssistant :true,
-    classList : ['chris', 'sarah', 'ebony', 'tom', 'charlie']
+  classList: ["chris", "sarah", "ebony", "tom", "charlie"],
+  hasTeachingAssistant: true,
 };
 let test1 = getStudents(classroomWithTeachingAssistant);
 console.log(test1);
 
 const classroomWithoutTeachingAssistant = {
-    hasTeachingAssistant :false,
-    classList : ['chris', 'sarah', 'ebony', 'tom', 'charlie']
-};let test2 = getStudents(classroomWithoutTeachingAssistant);
+  classList: ["chris", "sarah", "ebony", "tom", "charlie"],
+  hasTeachingAssistant: false,
+};
+let test2 = getStudents(classroomWithoutTeachingAssistant);
 console.log(test2);
