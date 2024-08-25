629f32a049a1b28e9f185bdb9865cfa8d8e82457
chris dexter <8195526+the-dext@users.noreply.github.com>
2024-08-25 09:19:51 +0100

    refactored to 1 method

--- start/01_12_string_permutation.js
+++ start/01_12_string_permutation.js
@@ -1,1 +1,19 @@
 // Write your code here
+
+function calcPermutations(stringInput) {
+  // factorial is n * n-1 * n-2...   where n is string length
+  let length =
+    typeof stringInput === "string" ? stringInput.length : stringInput;
+
+  if (length === 1) {
+    return 1;
+  } else {
+    return length * calcPermutations(length - 1);
+  }
+}
+
+// tests
+const stringInput = "123";
+// 3 * 2 * 1 = 6
+const result = calcPermutations(stringInput);
+console.log(result === 6 ? "passed" : `failed, result: ${result}`);
