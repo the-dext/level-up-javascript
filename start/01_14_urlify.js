c04a6ff2a24b92cdc587743d09bf9c6350f1a700
chris dexter <8195526+the-dext@users.noreply.github.com>
2024-08-25 12:27:50 +0100

    challenge 14

--- start/01_14_urlify.js
+++ start/01_14_urlify.js
@@ -1,1 +1,22 @@
 // Write your code here
+const convertToUrl = (blogTitle) => {
+  let url = blogTitle;
+
+  // remove punctuation
+  // trim
+  // lowercase
+  // replace spaces with hyphens.
+  return (
+    url
+      .trim()
+      .replace([" "], "-")
+      .toLowerCase()
+
+      //  .replace(/[!@£#$%^&*()\]\\/]/g, "");
+      .replace(/[^a-z0-9-]/g, "")
+  );
+};
+
+const title = "chris-LearNing javacsript]!@£#$%^&*()]\\/ ";
+const url = convertToUrl(title);
+console.log(title + " as url is: " + url);
