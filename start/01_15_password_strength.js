9fcde5422c5db0b567fb4a6c8c1f1f9c1d6b205f
chris dexter <8195526+the-dext@users.noreply.github.com>
2024-08-25 13:20:39 +0100

    refactored to use tegex test

--- start/01_15_password_strength.js
+++ start/01_15_password_strength.js
@@ -2,38 +2,46 @@
 function checkPasswordStrength(password) {
   let messages = [];
 
-  // at least one lowercase letter
-  // at least one upper case letter
-  // exactly one digit
-  // exactly one special char
   // at least 8 chars long
 
+  // at least one lowercase letter
   const lowerCasesRegEx = /[a-z]/g;
-  const lowerCaseCount = (password.match(lowerCasesRegEx) ?? '').length;
+  const lowerCaseCount = lowerCasesRegEx.test(password);
 
+  // at least one upper case letter
   const upperCasesRegEx = /[A-Z]/g;
-  const upperCaseCount = (password.match(upperCasesRegEx) ?? '').length;
+  const upperCaseCount = upperCasesRegEx.test(password);
 
+  // exactly one digit
   const digitRegex = /[0-9]/g;
-  const digitCount = (password.match(digitRegex) ?? '').length;
+  const digitCount = (password.match(digitRegex) ?? "").length == 1;
 
+  // exactly one special char
   const specialCharRegex = /[!@£#$%^&*()]/g;
-  const specialCharCount = (password.match(specialCharRegex) ?? '').length;
+  const specialCharCount = (password.match(specialCharRegex) ?? "").length == 1;
 
-  if (lowerCaseCount < 1)
+  if (!lowerCaseCount) {
     messages.push("password must contain at least 1 lowercase letter");
-  if (upperCaseCount < 1)
+  }
+  if (!upperCaseCount) {
     messages.push("password must contain at least 1 uppercase letter");
-  if (digitCount !== 1) messages.push("password must contain 1 digit");
-  if (specialCharCount !== 1)
+  }
+  if (!digitCount) {
+    messages.push("password must contain 1 digit");
+  }
+  if (!specialCharCount) {
     messages.push("password must contain 1 special character");
-  if (password.length < 8) messages.push("password must be at least 8 characters");
+  }
+  if (password.length < 8) {
+    messages.push("password must be at least 8 characters");
+  }
 
   const isPasswordOK = messages.length === 0;
   return [isPasswordOK, ...messages];
 }
 
 const strongPassword = "Pa$sWo0rd";
+const badPassword = "BAD";
 const [ok, ...messages] = checkPasswordStrength(strongPassword);
 
 console.log(ok ? "password is strong" : "password is weak");
